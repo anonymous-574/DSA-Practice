a34331bcce3da82b110a33ddbd84bdaf68faf97d
anonymous-574 <140256100+anonymous-574@users.noreply.github.com>
2024-08-06 09:40:05 +0530

    Add files via upload

---
+++ expt_0/binary.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    int no;
+    printf("Enter number: ");
+    scanf("%d" , &no);
+    int ans[4];
+
+    int i=0;
+    while (no >0)
+    {
+        ans[i]= (no%2);
+        no=no/2;
+        i++;
+    }
+    int size = sizeof(ans) / sizeof(ans[0]);
+    
+    int final[4];
+    
+    int reversedArr[size];
+    for (int i = 0; i < size; i++) {
+        reversedArr[i] = ans[size - i - 1];
+    }
+
+    // Print reversed array
+    printf("Binary number: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", reversedArr[i]);
+    }
+    
+
+    return 0;
+}
