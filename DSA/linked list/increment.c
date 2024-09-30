341c20ae7fe7e088cb540cdfe1bf4ba22c6e702e
Anonymous <daviddaniels778778@gmail.com>
2024-09-30 20:00:50 +0530

    new file:   DSA/linked list/increment.c 	new file:   DSA/trees/expression/expression.c 	Lab/expt_10/ 	Lab/expt_5/ 	Lab/expt_6/ 	Lab/expt_7/ 	Lab/expt_8/ 	Lab/expt_9/

---
+++ DSA/linked list/increment.c
@@ -0,0 +1,11 @@
+/*
+Incrementing a number
+
+Given as input a number as a singly linked list , output number + 1 as a singly linked list.
+
+Avoid using any  data structure other than singly linked list. 
+
+
+Sample input: 7 -> 9 -> 9 -> 9     Output: 8 ->  0 -> 0 -> 0
+*/
+
