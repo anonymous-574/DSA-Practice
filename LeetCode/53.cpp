936930be7f8feb2344e90cd9425ad868395602c5
anonymous-574 <daviddaniels778778@gmail.com>
2025-02-13 22:31:48 +0530

    modified:   LeetCode/53.cpp

--- LeetCode/53.cpp
+++ LeetCode/53.cpp
@@ -35,3 +35,16 @@ class Solution {
             return max_sum;
         }
     };
+
+
+//optimise ++
+class Solution {
+    public:
+        int maxSubArray(vector<int>& nums) {
+            int curMax = 0, maxTillNow = INT_MIN;
+            for(int c : nums)
+                curMax = max(c, curMax + c),
+                maxTillNow = max(maxTillNow, curMax);
+            return maxTillNow;
+        }
+    };
